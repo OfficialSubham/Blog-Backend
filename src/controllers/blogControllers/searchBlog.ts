9d15f1458716a023033b2b10886d006475f2f3d7
Subham Mondal <subhamoffi@gmail.com>
2025-03-22 14:31:54 +0530

    Solved the typo problem in search of blog

--- src/controllers/blogControllers/searchBlog.ts
+++ src/controllers/blogControllers/searchBlog.ts
@@ -9,38 +9,46 @@ export const searchBlogs = async (c: Context) => {
         console.log(query)
         const prisma = new PrismaClient({ datasourceUrl: c.env.DATABASE_URL }).$extends(withAccelerate())
         if (!query) {
-            const blogs = await prisma.blog.findMany({})
-            return c.json({message: "Here is all blog", blogs})
+            const blogs = await prisma.blog.findMany({
+                include: {
+                    user: true
+                }
+            })
+            return c.json({ message: "Here is all blog", blogs })
         }
-        const relatedBlogs = await prisma.blog.findMany({
+        const blogs = await prisma.blog.findMany({
             where: {
                 OR: [{
-                    title: {contains: query, mode: "insensitive"}
+                    title: { contains: query, mode: "insensitive" }
                 }, {
-                    description: {contains: query, mode: "insensitive"}
+                    description: { contains: query, mode: "insensitive" }
                 }],
                 deleted: false
             },
             include: {
-                user: true
+                user: {
+                    omit: {
+                        password: true
+                    }
+                }
             }
         })
-        if(!relatedBlogs[0]) {
-            return c.json({message: "There is no blog"}, 400)
+        if (!blogs[0]) {
+            return c.json({ message: "There is no blog" }, 400)
         }
-        return c.json({message: "Here is all blog", relatedBlogs})
+        return c.json({ message: "Here is all blog", blogs })
     } catch (error) {
-        return c.json({message: "Internal error occured"}, 500)
+        return c.json({ message: "Internal error occured" }, 500)
     }
 }
 
 export const searchBlogWithTag = async (c: Context) => {
     try {
-    const {tag} = await c.req.json()
+        const { tag } = await c.req.json()
         if (!tag[0]) {
             return c.json({ message: "Enter valid Tag" }, 400)
         }
-        const insensitiveTag = tag.map((tag:string) => tag.toLowerCase())
+        const insensitiveTag = tag.map((tag: string) => tag.toLowerCase())
         const prisma = new PrismaClient({ datasourceUrl: c.env.DATABASE_URL }).$extends(withAccelerate())
         const relatedTagBlogs = await prisma.blog.findMany({
             where: {
@@ -50,11 +58,11 @@ export const searchBlogWithTag = async (c: Context) => {
                 deleted: false
             }
         })
-        if(!relatedTagBlogs[0]) {
-            return c.json({message: "There is no blog"}, 400)
+        if (!relatedTagBlogs[0]) {
+            return c.json({ message: "There is no blog" }, 400)
         }
-        return c.json({message: "Here is all blog", relatedTagBlogs})
+        return c.json({ message: "Here is all blog", relatedTagBlogs })
     } catch (error) {
-        return c.json({message: "Internal error occured"}, 500)
+        return c.json({ message: "Internal error occured" }, 500)
     }
 }
